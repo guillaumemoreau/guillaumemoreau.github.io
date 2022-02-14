61fbe307045ca9086f063aa8f6a22904498ca2d3
Guillaume Moreau <guillaume.moreau@imt-atlantique.fr>
2022-02-14 14:37:38 +0100

    Update site

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 bundle exec jekyll build --config _config.yml,_config_github.yml
-'cp' -aRf _site/* ../ && git add ../ && git commit -m "Update site" && git push origin master
+'cp' -aRf _site/* ../ && git add ../ && git commit -m "Update site" 
 
